d739b57e697c05a47fedd949d58c432a4fadd816
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-18 21:23:34 +0900

    チャレンジ問題 回答

--- importing-and-exporting-components/src/Gallery.tsx
+++ importing-and-exporting-components/src/Gallery.tsx
@@ -1,12 +1,5 @@
-// Move me to Profile.js!
-export function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/QIrZWGIs.jpg"
-      alt="Alan L. Hart"
-    />
-  );
-}
+// import Profile from "./Profile.js"; // default exportバージョン
+import { Profile } from "./Profile.js";
 
 export default function Gallery() {
   return (
