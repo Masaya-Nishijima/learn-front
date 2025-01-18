53fca8213ba47c002acf4c0f490a03fc2bdd6919
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-18 18:30:52 +0900

    チャレンジ3 回答

--- your-first-component_3/src/App.tsx
+++ your-first-component_3/src/App.tsx
@@ -1,4 +1,4 @@
-function profile() {
+function Profile() {
   return (
     <img
       src="https://i.imgur.com/QIrZWGIs.jpg"
@@ -11,9 +11,9 @@ export default function Gallery() {
   return (
     <section>
       <h1>Amazing scientists</h1>
-      <profile />
-      <profile />
-      <profile />
+      <Profile />
+      <Profile />
+      <Profile />
     </section>
   );
 }
