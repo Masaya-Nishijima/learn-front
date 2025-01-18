6ac1a76427ea3fca4b8f7fb8bece91f780ddceaf
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-18 19:59:25 +0900

    プロジェクト作成 & 開発サーバー?を起動するスクリプトを実装

---
+++ auto_project.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+PROJECT_NAME=$1
+if [ "$#" -eq 1 ]; then
+  if [ ! -e $PROJECT_NAME ]; then
+    yarn create vite $PROJECT_NAME -t react-swc-ts
+    (cd $PROJECT_NAME/src && rm -r assets App.css App.tsx index.css && touch App.tsx && sed -i -e "/index.css'/d" main.tsx)
+  fi
+  (cd $PROJECT_NAME && yarn && yarn dev)
+else
+  echo "引数は1つです(プロジェクト名を入力)"
+fi
