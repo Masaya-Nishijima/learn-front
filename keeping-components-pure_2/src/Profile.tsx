66f617275627107821d1b2d12bae58031c65e970
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-19 17:11:42 +0900

    チャレンジ2 回答

--- keeping-components-pure_2/src/Profile.tsx
+++ keeping-components-pure_2/src/Profile.tsx
@@ -1,28 +1,27 @@
 import Panel from './Panel.tsx';
+import { Person } from './types.ts';
 import { getImageUrl } from './utils.ts';
 
-let currentPerson;
 
-export default function Profile({ person }) {
-  currentPerson = person;
+export default function Profile({ person }: {person: Person}) {
   return (
     <Panel>
-      <Header />
-      <Avatar />
+      <Header name={person.name} />
+      <Avatar person={person}/>
     </Panel>
   )
 }
 
-function Header() {
-  return <h1>{currentPerson.name}</h1>;
+function Header({name}: {name: string}) {
+  return <h1>{name}</h1>;
 }
 
-function Avatar() {
+function Avatar({person}: {person: Person}) {
   return (
     <img
       className="avatar"
-      src={getImageUrl(currentPerson)}
-      alt={currentPerson.name}
+      src={getImageUrl(person)}
+      alt={person.name}
       width={50}
       height={50}
     />
