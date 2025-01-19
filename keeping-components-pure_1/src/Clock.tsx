a3b09141e2123d7717e5a58d43d9c171d32b0579
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-19 16:53:24 +0900

    チャレンジ1 設問

---
+++ keeping-components-pure_1/src/Clock.tsx
@@ -0,0 +1,14 @@
+export default function Clock({ time }: { time: Date}) {
+  // eslint-disable-next-line prefer-const
+  let hours = time.getHours();
+  if (hours >= 0 && hours <= 6) {
+    document.getElementById('time').className = 'night';
+  } else {
+    document.getElementById('time').className = 'day';
+  }
+  return (
+    <h1 id="time">
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
