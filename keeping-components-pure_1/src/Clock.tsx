c8b32dfb07dc87dad2a5e47b1ec10022ae70f0dd
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-19 16:56:04 +0900

    チャレンジ1 回答(若干ズル)
    最初、return内で三項演算子でごちゃごちゃやる方法を思いついたが、
    模範回答をチラ見して、変数に保存する方法に変更した

--- keeping-components-pure_1/src/Clock.tsx
+++ keeping-components-pure_1/src/Clock.tsx
@@ -1,13 +1,14 @@
 export default function Clock({ time }: { time: Date}) {
   // eslint-disable-next-line prefer-const
   let hours = time.getHours();
+  let className;
   if (hours >= 0 && hours <= 6) {
-    document.getElementById('time').className = 'night';
+    className = 'night';
   } else {
-    document.getElementById('time').className = 'day';
+    className = 'day';
   }
   return (
-    <h1 id="time">
+    <h1 id="time" className={className}>
       {time.toLocaleTimeString()}
     </h1>
   );
