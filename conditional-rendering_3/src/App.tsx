cf4dd78b89b93bf4b8b59961f4c5320efef4d779
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-01-19 13:08:57 +0900

    チャレンジ3 回答

--- conditional-rendering_3/src/App.tsx
+++ conditional-rendering_3/src/App.tsx
@@ -1,14 +1,30 @@
 function Drink({ name }) {
+  let drinkInfo
+  if (name === 'tea') {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    drinkInfo = {
+      part: 'leaf',
+      caffein: '15–70 mg/cup',
+      age: '4,000+ years'
+    }
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    drinkInfo = {
+      part: 'bean',
+      caffein: '80–185 mg/cup',
+      age: '1,000+ years'
+    }
+  }
   return (
     <section>
       <h1>{name}</h1>
       <dl>
         <dt>Part of plant</dt>
-        <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+        <dd>{drinkInfo.part}</dd>
         <dt>Caffeine content</dt>
-        <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
+        <dd>{drinkInfo.caffein}</dd>
         <dt>Age</dt>
-        <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
+        <dd>{drinkInfo.age}</dd>
       </dl>
     </section>
   );
