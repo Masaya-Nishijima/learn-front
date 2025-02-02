7e6af1e935dfa56e6fcb93abd769512f76ada5d1
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-02-02 17:16:51 +0900

    チャレンジ3 回答

--- synchronizing-with-effects_3/src/Counter.tsx
+++ synchronizing-with-effects_3/src/Counter.tsx
@@ -8,7 +8,10 @@ export default function Counter() {
       setCount(c => c + 1);
     }
 
-    setInterval(onTick, 1000);
+    const intervalID = setInterval(onTick, 1000);
+    return () => {
+      clearInterval(intervalID)
+    }
   }, []);
 
   return <h1>{count}</h1>;
