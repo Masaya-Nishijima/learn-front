9c335e5f559d934251fb90fd3f7c660ddada7636
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-02-02 16:58:17 +0900

    チャレンジ4 回答
    → 自力で動く状態まで持っていけず、ほぼ写経

--- manipulating-the-dom-with-refs_4/src/App.tsx
+++ manipulating-the-dom-with-refs_4/src/App.tsx
@@ -1,13 +1,16 @@
+import { useRef } from 'react';
 import SearchButton from './SearchButton';
 import SearchInput from './SearchInput';
 
 export default function Page() {
+  const inputRef = useRef();
+
   return (
     <>
       <nav>
-        <SearchButton />
+        <SearchButton onClick={() => {inputRef.current.focus()}} />
       </nav>
-      <SearchInput />
+      <SearchInput ref={inputRef} />
     </>
   );
 }
--- manipulating-the-dom-with-refs_4/src/SearchInput.tsx
+++ manipulating-the-dom-with-refs_4/src/SearchInput.tsx
@@ -1,7 +1,12 @@
-export default function SearchInput() {
-  return (
-    <input
-      placeholder="Looking for something?"
-    />
-  );
-}
+import { forwardRef } from "react";
+
+export default forwardRef(
+  function SearchInput(props, ref) {
+    return (
+      <input
+        ref={ref}
+        placeholder="Looking for something?"
+      />
+    );
+  }
+)
