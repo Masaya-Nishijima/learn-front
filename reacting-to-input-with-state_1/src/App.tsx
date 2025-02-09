f52c2b5b0a4749066e791ef5917440bcc29acdfd
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-02-09 16:02:45 +0900

    チャレンジ1 回答

--- reacting-to-input-with-state_1/src/App.tsx
+++ reacting-to-input-with-state_1/src/App.tsx
@@ -1,29 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Picture() {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false)
 
-  let backgroundClassName = 'background';
-  let pictureClassName = 'picture';
-  if (isActive) {
-    pictureClassName += ' picture--active';
-  } else {
-    backgroundClassName += ' background--active';
+  function handleClickBackground() {
+    setIsActive(false);
+  }
+
+  function handleClickPicture(e) {
+    e.stopPropagation();
+    setIsActive(true);
   }
 
   return (
     <div
-      className={backgroundClassName}
-      onClick={() => setIsActive(false)}
+      className={isActive ?  "background": "background background--active"}
+      onClick={handleClickBackground}
     >
       <img
-        onClick={e => {
-          e.stopPropagation();
-          setIsActive(true);
-        }}
-        className={pictureClassName}
+        className={isActive ? "picture picture--active" : "picture"}
         alt="Rainbow houses in Kampung Pelangi, Indonesia"
         src="https://i.imgur.com/5qwVYb1.jpeg"
+        onClick={handleClickPicture}
       />
     </div>
   );
