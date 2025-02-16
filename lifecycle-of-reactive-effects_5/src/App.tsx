86b2c13f60d4da7a9f2993f69851a5b4bda064a5
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2025-02-16 14:12:37 +0900

    チャレンジ5 回答
    模範回答コピペ

--- lifecycle-of-reactive-effects_5/src/App.tsx
+++ lifecycle-of-reactive-effects_5/src/App.tsx
@@ -1,26 +1,18 @@
-import { useState, useEffect } from 'react';
-import { fetchData } from './api.js';
+import { useState } from 'react';
+import { useSelectOptions } from './useSelectOptions.js';
 
 export default function Page() {
-  const [planetList, setPlanetList] = useState([])
-  const [planetId, setPlanetId] = useState('');
+  const [
+    planetList,
+    planetId,
+    setPlanetId
+  ] = useSelectOptions('/planets');
 
-  const [placeList, setPlaceList] = useState([]);
-  const [placeId, setPlaceId] = useState('');
-
-  useEffect(() => {
-    let ignore = false;
-    fetchData('/planets').then(result => {
-      if (!ignore) {
-        console.log('Fetched a list of planets.');
-        setPlanetList(result);
-        setPlanetId(result[0].id); // Select the first planet
-      }
-    });
-    return () => {
-      ignore = true;
-    }
-  }, []);
+  const [
+    placeList,
+    placeId,
+    setPlaceId
+  ] = useSelectOptions(planetId ? `/planets/${planetId}/places` : null);
 
   return (
     <>
@@ -29,7 +21,7 @@ export default function Page() {
         <select value={planetId} onChange={e => {
           setPlanetId(e.target.value);
         }}>
-          {planetList.map(planet =>
+          {planetList?.map(planet =>
             <option key={planet.id} value={planet.id}>{planet.name}</option>
           )}
         </select>
@@ -39,13 +31,13 @@ export default function Page() {
         <select value={placeId} onChange={e => {
           setPlaceId(e.target.value);
         }}>
-          {placeList.map(place =>
+          {placeList?.map(place =>
             <option key={place.id} value={place.id}>{place.name}</option>
           )}
         </select>
       </label>
       <hr />
-      <p>You are going to: {placeId || '???'} on {planetId || '???'} </p>
+      <p>You are going to: {placeId || '...'} on {planetId || '...'} </p>
     </>
   );
 }
